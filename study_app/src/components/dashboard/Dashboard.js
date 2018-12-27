c6e8ec04d7b77a645e62c0595856691ed6b2fcdd
ToMMaaY <thuynh629@gmail.com>
2018-12-26 16:16:22 -0800

    Created a Favorites List component

---
+++ study_app/src/components/dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m6 l4">
+            <div className="card">
+              <h3>Favorites</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
